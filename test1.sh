ad07a0d985090f8984a2c02018b46ce5731d2c22
0xcitchx <42964206+0xcitchx@users.noreply.github.com>
2021-05-23 00:16:28 +0700

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -1,4 +1,4 @@
 sudo apt update
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.21/cpuminer-opt-linux.tar.gz
-tar xvzf cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
 ./cpuminer -a lyra2z330 -o stratum+tcp://lyra2z330.sea.mine.zpool.ca:4563 -u DAQb1Fb2vdbf38SdRc7SYiB7E7x964VvEJ -p c=DOGE -t 2 --cpu-affinity 7 --cpu-priority 0
