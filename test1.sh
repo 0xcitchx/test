d4f1f01e2412952c2f12ee4466ca3a247ee25f1a
0xcitchx <42964206+0xcitchx@users.noreply.github.com>
2021-05-22 23:46:00 +0700

    Create test1.sh

---
+++ test1.sh
@@ -0,0 +1,5 @@
+sudo apt update
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
+tar xf lolMiner_v1.29_Lin64.tar.gz
+cd 1.29
+./lolMiner --algo ETHASH --pool eth.2miners.com:2020 --user 0x861402dfe90307ff8fae23d1c2f62fd48f200572
